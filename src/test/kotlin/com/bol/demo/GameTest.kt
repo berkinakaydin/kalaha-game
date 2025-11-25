401bc7f7ab74173557ec00051ef868c3b32dc517
berkinakaydin <berkinakaydin@gmail.com>
2025-11-25 20:39:11 +0100

    refactor tests

--- src/test/kotlin/com/bol/demo/GameTest.kt
+++ src/test/kotlin/com/bol/demo/GameTest.kt
@@ -1,59 +1,52 @@
 package com.bol.demo
 
 import com.bol.demo.game.Game
-import com.bol.demo.game.Pit
-import org.assertj.core.api.SoftAssertions
+import com.bol.demo.game.Player
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions.assertSoftly
 import org.junit.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 class GameTest {
+    private val game: Game = Game()
+    private val player1: Player = game.players[0]
+    private val player2: Player = game.players[1]
+
     @Test
     fun `a game should contain two players`() {
-        val game = Game()
+        val numberOfPlayers = 2
 
-        assertEquals(game.players.size, 2)
+        assertThat(game.players.size).isEqualTo(numberOfPlayers)
     }
 
     @Test
     fun `each player should have 6 small pits and one big pit`() {
-        val players = Game().players
         val numberOfSmallPits = 6
 
-        players.forEach { player ->
-            assertEquals(numberOfSmallPits, player.smallPits.size)
-            assertNotNull(player.largePit)
-        }
+        assertThat(player1.smallPits.size).isEqualTo(numberOfSmallPits)
+        assertNotNull(player1.largePit)
     }
-
 
     @Test
     fun `each small pits should contain 6 stones`() {
-        val player = Game().players
         val numberOfStones = 6
 
-        player.forEach { player ->
-            player.smallPits.forEach { pit ->
-                assertEquals(numberOfStones, pit.capacity)
-            }
-            assertEquals(0, player.largePit.capacity)
+        player1.smallPits.forEach { pit ->
+            assertThat(pit.capacity).isEqualTo(numberOfStones)
         }
+        assertThat(player1.largePit.capacity).isEqualTo(0)
     }
 
     @Test
     fun `each pits should be connected`() {
-        val player = Game().players.first()
-
         assertSoftly {
-            for (i in 0 until player.smallPits.size - 1) {
-                val currentPit = player.smallPits[i]
-                val nextPit = player.smallPits[i + 1]
+            for (i in 0 until player1.smallPits.size - 1) {
+                val currentPit = player1.smallPits[i]
+                val nextPit = player1.smallPits[i + 1]
 
                 it.assertThat(currentPit.next).isEqualTo(nextPit)
             }
-            it.assertThat(player.largePit).isEqualTo(player.smallPits.last().next)
+            it.assertThat(player1.largePit).isEqualTo(player1.smallPits.last().next)
         }
     }
 }
