c4d38c2d73262ea9750d9c2785e527d390552327
berkinakaydin <berkinakaydin@gmail.com>
2025-11-25 17:25:00 +0100

    [RED] players should have 6 pits and a large pit

--- src/test/kotlin/com/bol/demo/GameTest.kt
+++ src/test/kotlin/com/bol/demo/GameTest.kt
@@ -2,7 +2,9 @@ package com.bol.demo
 
 import com.bol.demo.game.Game
 import org.junit.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class GameTest {
     @Test
@@ -10,5 +12,15 @@ class GameTest {
         val game = Game()
 
         assertEquals(game.players.size, 2)
+    }
+
+    @Test
+    fun `each player should have 6 pits and a large pit`(){
+        val players = Game().players
+
+        players.forEach { player ->
+            assertEquals(player.pits.size, 6)
+            assertNotNull(player.largePit)
+        }
     }
 }
