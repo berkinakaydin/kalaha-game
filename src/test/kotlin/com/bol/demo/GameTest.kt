05f6cf1f4f7f9c6fea254edbe9189b3c730202bc
berkinakaydin <berkinakaydin@gmail.com>
2025-11-25 20:12:01 +0100

    [RED] pits should be connected

--- src/test/kotlin/com/bol/demo/GameTest.kt
+++ src/test/kotlin/com/bol/demo/GameTest.kt
@@ -1,6 +1,9 @@
 package com.bol.demo
 
 import com.bol.demo.game.Game
+import com.bol.demo.game.Pit
+import org.assertj.core.api.SoftAssertions
+import org.assertj.core.api.SoftAssertions.assertSoftly
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -26,7 +29,6 @@ class GameTest {
     }
 
 
-
     @Test
     fun `each small pits should contain 6 stones`() {
         val player = Game().players
@@ -37,6 +39,21 @@ class GameTest {
                 assertEquals(numberOfStones, pit.capacity)
             }
             assertEquals(0, player.largePit.capacity)
+        }
+    }
+
+    @Test
+    fun `each pits should be connected`() {
+        val player = Game().players.first()
+
+        assertSoftly {
+            for (i in 0 until player.smallPits.size - 1) {
+                val currentPit = player.smallPits[i]
+                val nextPit = player.smallPits[i + 1]
+
+                it.assertThat(currentPit.next).isEqualTo(nextPit)
+            }
+            it.assertThat(player.largePit).isEqualTo(player.smallPits.last().next)
         }
     }
 }
