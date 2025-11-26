af4adfae91ced24613542726a20649717fa250e1
berkinakaydin <berkinakaydin@gmail.com>
2025-11-26 02:45:20 +0100

    [GREEN] while sowing the stones, current players pits might be changed more than once

--- src/test/kotlin/com/bol/demo/GameTest.kt
+++ src/test/kotlin/com/bol/demo/GameTest.kt
@@ -4,13 +4,21 @@ import com.bol.demo.game.Game
 import com.bol.demo.game.Player
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions.assertSoftly
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import kotlin.test.assertNotNull
 
 class GameTest {
-    private val game: Game = Game()
-    private val player1: Player = game.player1
-    private val player2: Player = game.player2
+    private lateinit var game: Game
+    private lateinit var player1: Player
+    private lateinit var player2: Player
+
+    @BeforeEach
+    fun setUp(){
+        game = Game()
+        player1 = game.player1
+        player2 = game.player2
+    }
 
     @Test
     fun `a game should contain two players`() {
@@ -68,7 +76,7 @@ class GameTest {
         game.makeMove(chosenPitIndex)
         assertThat(player1.smallPits[chosenPitIndex].capacity).isEqualTo(0)
 
-        repeat(numberOfStones){ i ->
+        repeat(numberOfStones){
             val newPit = currentPit.next
             val oldPit = oldStateOfCurrentPit.next
             assertThat(newPit.capacity).isEqualTo(oldPit.capacity + 1)
@@ -87,5 +95,16 @@ class GameTest {
 
         game.makeMove(chosenPitIndex)
         assertThat(player2.largePit.capacity).isEqualTo(oldStateOfOpponentPlayer.largePit.capacity)
+    }
+
+    @Test
+    fun `while sowing the stones, current players pits might be changed more than once`(){
+        val chosenPitIndex = 0
+        val currentPit = player1.smallPits[chosenPitIndex]
+        currentPit.capacity = 36 //when it is at least 18, that means opponent's pits get stones
+
+        game.makeMove(chosenPitIndex)
+
+        assertThat(currentPit.capacity).isEqualTo(2)
     }
 }
